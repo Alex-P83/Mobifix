ae4d6f7c93b0a96087756da747d1466900c470c6
Alex <topweb1983@gmail.com>
2019-06-16 16:56:26 +0300

    Start change product cart

--- app/js/main.js
+++ app/js/main.js
@@ -8,3 +8,35 @@ $('.main-slider').slick({
     nextArrow: '<i class="fas fa-chevron-right slick-next"></i>',
     prevArrow: '<i class="fas fa-chevron-left slick-prev"></i>'
   });
+
+ // Menu button
+ (function() {
+  "use strict";
+  var toggles = document.querySelectorAll(".c-hamburger");
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+    });
+  }
+})();
+
+///Drop menu
+$(function(){
+  $('.colapse-menu-button .c-hamburger').on('click',function(){
+    $('header .order_block-2').slideToggle();
+  });
+});
+
+// Hidden menu
+var menu = $('header .menu'); 
+$(window).resize(function(){
+    var wid = $(window).width();
+    if(wid > 570 && menu.is(':hidden')) {
+        menu.removeAttr('style');
+    } 
+});   
